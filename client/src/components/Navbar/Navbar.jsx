b0c0fdc08ad988501d0e222eea5dc64906da7140
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2023-01-12 23:28:04 +0530

    Navbar done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,34 @@
 import "./Navbar.scss";
-import {Button } from '@web3uikit/core';
-import { ConnectButton } from 'web3uikit';
+import { ConnectButton, Dropdown } from 'web3uikit';
 import Ducklogo from '../../assets/images/ducklogo.png'
 
-const Navbar = ()=>{
+const Navbar = () => {
     return <div className="Navbar">
         <div className="row">
-            <div className="col-6 navcol1">
-                <span className="logospan">
-                    <img src = {Ducklogo} className = 'logo'></img>
+            <div className="col-8 navcol1">
+                <span className="logospan d-none d-sm-inline">
+                    <img src={Ducklogo} className='logo' role="button" id="navbarDropdown" data-bs-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false"></img>
                     <h3 className="logotext">
                         Duckounting
                     </h3>
                 </span>
-                <button type="button" class="btn infobutton">Learn More</button>
+                <button type="button" class="btn infobutton d-none d-sm-inline">Learn More</button>
+
+                <a className="nav-link dropdown-toggle d-inline d-sm-none" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src={Ducklogo} className='logodrop' role="button" id="navbarDropdown" data-bs-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false"></img>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <h3 className="logotext droplogo">
+                        Duckounting
+                    </h3>
+                    <a class="dropdown-item" href="#">Learn More</a>
+                    {/* <a class="dropdown-item" href="#">Something else here</a> */}
+                </div>
             </div>
-            <div className="col-6 navcol2">
-                <ConnectButton moralisAuth={true} className ="connect"/>
+            <div className="col-4 navcol2">
+                <ConnectButton moralisAuth={true} color="yellow" className="connect" />
             </div>
         </div>
     </div>
