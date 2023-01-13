8eeff60f129fdf23530de09673995cfea7743f48
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2023-01-13 15:09:24 +0530

    Login stub

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,44 @@
 import "./Navbar.scss";
-import { ConnectButton, Dropdown } from 'web3uikit';
+import { ConnectButton, Dropdown, Input } from 'web3uikit';
 import Ducklogo from '../../assets/images/ducklogo.png'
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
 
 const Navbar = () => {
+    const [showLogin, setShowLogin] = useState(false);
+
+    const connectWallet = () => {
+        setShowLogin(true);
+    }
+    const hideLogin = () => {
+        setShowLogin(false);
+    }
+
+    const [pan, setPan] = useState("");
+    const [name, setName] = useState("");
+
+    const handlePan = event => {
+        setPan(event.target.value);
+    }
+    const handleName = event => {
+        setName(event.target.value);
+    }
+
+    const submitHandler = () => {
+        console.log(pan);
+        console.log(name);
+        hideLogin();
+    }
+
     return <div className="Navbar">
         <div className="row">
             <div className="col-8 navcol1">
                 <span className="logospan d-none d-sm-inline">
                     <img src={Ducklogo} className='logo' role="button" id="navbarDropdown" data-bs-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false"></img>
-                    <h3 className="logotext">
+                    {/* <h3 className="logotext">
                         Duckounting
-                    </h3>
+                    </h3> */}
                 </span>
                 <button type="button" class="btn infobutton d-none d-sm-inline">Learn More</button>
 
@@ -28,10 +55,33 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="col-4 navcol2">
-                <ConnectButton moralisAuth={true} color="yellow" className="connect" />
+                <ConnectButton moralisAuth={true} color="yellow" className="connect"
+                    onClick={connectWallet}
+                />
             </div>
         </div>
-    </div>
+
+        {/* login modal */}
+        <Modal show={showLogin} onHide={hideLogin} className = 'loginModal'>
+            <Modal.Title className="loginTitle">We need some deets...</Modal.Title>
+            <Modal.Body className ="loginBody">
+                <label for="nameinp">Name</label>
+                <input type="text" class="form-control" id="nameinp" placeholder="Ducky Duckington" onChange = {handleName}/>
+                <div className="space"></div>
+                <label for="paninp">PAN Number</label>
+                <input type="text" class="form-control" id="paninp" placeholder="DUCKS9669D" onChange={handlePan}/>
+                <div className="row btnrow">
+                    <button className="btn modbuttons" onClick={hideLogin}>
+                        Skip
+                    </button>
+                    <button className="btn modbuttons">
+                        Save Locally
+                    </button>
+                </div>
+            </Modal.Body>
+        </Modal>
+
+    </div >
 }
 
 export default Navbar;
