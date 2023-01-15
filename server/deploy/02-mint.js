ed1881d85a3eb2ade06227979093bd9c88c4691b
RohitShah1706 <rlshah03@gmail.com>
2023-01-16 01:35:37 +0530

    fixed conflict - env to mint - check mint test if allowing to mint - change tokenId 1->2

--- server/deploy/02-mint.js
+++ server/deploy/02-mint.js
@@ -5,15 +5,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
-    const invoicePlatform = await ethers.getContract("InvoicePlatform", deployer)
-    console.log(`Executing InvoicePlatform.registerPerson() on ${chainId}`)
-    await invoicePlatform.registerPerson(
-        "Rohit PAN", "Rohit Shah"
-    );            
-    console.log(`InvoicePlatform.registerPerson() executed successfully on ${chainId}`)
-    console.log(`Minted 1 NFT for sellerPAN and sellerName: ${
-        await invoicePlatform.tokenURI(1)
-    }`);
+    if(process.env.MINT_NFT == "true") {
+        const invoicePlatform = await ethers.getContract("InvoicePlatform", deployer)
+        console.log(`Executing InvoicePlatform.registerPerson() on ${chainId}`)
+        await invoicePlatform.registerPerson(
+            "Rohit PAN", "Rohit Shah"
+        );            
+        console.log(`InvoicePlatform.registerPerson() executed successfully on ${chainId}`)
+        console.log(`Minted 1 NFT for sellerPAN and sellerName: ${
+            await invoicePlatform.tokenURI(1)
+        }`);
+    }
+    else{
+        console.log(`Minting NFT skipped on ${chainId}`)
+    }
 
 
 }
